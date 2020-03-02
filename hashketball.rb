6bad8b58f158916ce99efe1d2dde5a209a398d3a
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-02 00:57:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -260,7 +260,7 @@ def player_with_longest_name
       if long_name < attributes[:player_name].length
         long_name = attributes[:player_name].length
       if long_name == attributes[:player_name].length
-          longest_player.slice(attributes[:player_name])
+      longest_player.slice(attributes[:player_name])
       longest_player << attributes[:player_name]
   
     end
